c224beba7089c34230d39c2981099684756609db
Diana <biktagirovad@yandex.ru>
2024-10-06 22:24:32 +0300

    Описание, что вы комитите

---
+++ sem1/BiktagirovaDiana/MiniHomeWork2/Task1.cpp
@@ -0,0 +1,29 @@
+﻿// Task1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+ 
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::oct;
+using std::hex;
+
+
+
+int DecimalNumber = 0;
+int OctalNumber = 0;
+int HexadecimalNumber = 0;
+
+
+
+int main() {
+
+	setlocale(LC_ALL, "Russian");
+	cout << "Введите десятичное число" << endl;
+	cin >> DecimalNumber;
+	cout << oct <<"в восьмеричной системе:" << DecimalNumber << endl;
+	cout << hex <<"в шестнадцатеричная система:" << DecimalNumber << endl;
+	return 0; 
+
+}
---
+++ sem1/BiktagirovaDiana/MiniHomeWork2/Task2.cpp
@@ -0,0 +1,31 @@
+﻿// Task2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+int n;
+void Sirakys()
+{
+    while (n != 1) {
+        if (n % 2 == 0) {
+            n = n / 2;
+        }
+        else if (n % 2 != 0) {
+            n = ((n * 3) + 1) / 2;
+
+        }
+
+    }
+    cout << n << endl;
+    cout << "Гипотеза подтверждена";
+}
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите натуральное число" << endl;
+    cin >> n;
+    Sirakys();
+}
