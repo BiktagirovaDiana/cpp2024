3f07ab9f4a8baea60802084752244c9169e1e1fc
BiktagirovaDiana <biktagirovad@yandex.ru>
2025-06-06 16:46:31 +0300

    1 мини дз

---
+++ sem2/minihomework1/MathClient.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <vector>
+#include "MathLibrary.h"
+
+using std::vector;
+using std::cout;
+using std::endl;
+
+int main() {
+    vector<double> numbers = { 1.0, 2.1, 3.0, 4.3, 5.0 };
+
+    double mean = CalculateMean(numbers);
+    cout << "Mean: " << mean << endl;
+    double median = CalculateMedian(numbers);
+    cout << "Median: " << median << endl;
+    double quadraticMean = CalculateQuadraticMean(numbers);
+    cout << "Quadratic Mean: " << quadraticMean << endl;
+    double variance = CalculateVariance(numbers);
+    cout << "Variance: " << variance << endl;
+    return 0;
+}
